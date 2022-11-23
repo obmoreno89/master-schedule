48f1d4a9a3e73604506c66a4a56014effae8f31a
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2022-11-23 16:18:18 -0600

    cambio de logos en login y sidebar

--- src/images/icon/icons.js
+++ src/images/icon/icons.js
@@ -1,7 +1,11 @@
 import openEye from './openEye.svg';
 import closedEye from './closedEye.svg';
+import logoNide from './logoNide.jpeg';
+import logoMotors from './logoMotors.png';
 
 export default {
   openEye,
   closedEye,
+  logoNide,
+  logoMotors,
 };
