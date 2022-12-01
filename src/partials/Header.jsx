1539633919fcc621a3090a0adee5e53b6554dffe
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2022-12-01 09:24:59 -0600

    cambio de altura del header

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -8,9 +8,9 @@ function Header({ sidebarOpen, setSidebarOpen }) {
 
   return (
     <>
-      <header className='sticky top-0 bg-white border-b border-slate-200 z-30'>
+      <header className='sticky top-0 bg-white border-b  border-slate-200 z-30'>
         <div className='px-4 sm:px-6 lg:px-8'>
-          <div className='flex items-center justify-between h-16 -mb-px'>
+          <div className='flex items-center justify-between h-12 -mb-px'>
             <div>NIDE | MP-PRO</div>
             {/* Header: Left side */}
             <div className='flex'>
