b400b11864d515c79e51fc5aaeb1e815f48239c6
Maximiliano Montaña <maxi.omr01@gmail.com>
2022-12-08 15:13:54 -0300

    se agrega conexion con endpoint de listado de lineas de productos

--- src/store/slice/capabilitiesSlice.js
+++ src/store/slice/capabilitiesSlice.js
@@ -1,31 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   groupList: [],
+  producLines: [],
 };
 
 const capabilitiesSlice = createSlice({
   initialState,
-  name: 'group',
+  name: "group",
   reducers: {
     setGroup: (state, action) => {
       state.groupList = action.payload;
     },
+    setProductLines: (state, action) => {
+      state.producLines = action.payload;
+    },
   },
 });
 
-export const { setGroup } = capabilitiesSlice.actions;
+export const { setGroup, setProductLines } = capabilitiesSlice.actions;
 
 export const selectGroup = (state) => state.group.groupList;
+export const selectPLines = (state) => state.group.producLines;
 
 export default capabilitiesSlice.reducer;
 
 export const getGroupList = () => (dispatch) => {
   axios
-    .get('http://44.211.175.241/api/capacities/list-groups')
+    .get("http://44.211.175.241/api/capacities/list-groups")
     .then((response) => {
       dispatch(setGroup(response.data));
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getProductLines = () => (dispatch) => {
+  axios
+    .get("http://44.211.175.241/api/capacities/list-product-line")
+    .then((response) => {
+      dispatch(setProductLines(response.data));
     })
     .catch((err) => console.log(err));
 };
