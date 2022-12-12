ad4d17971f098ba4d6f11c9988f9749931fc6dca
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2022-12-12 10:56:03 -0600

    paginacion en tabla de usuarios

--- src/partials/capabilities/CapabilitiesTable.jsx
+++ src/partials/capabilities/CapabilitiesTable.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import CapabilitiesTableItem from './CapabilitiesTableItem';
+import PaginationCapabilities from '../../components/PaginationCapabilities';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   selectCapabilitiesList,
@@ -7,57 +8,72 @@ import {
 } from '../../store/slice/capabilitiesSlice';
 
 const CapabilitiesTable = ({ setTransactionPanelOpen, setGroupPanelOpen }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
   const capabilitiesList = useSelector(selectCapabilitiesList);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getCapabilitiesList());
-  }, []);
+  }, [capabilitiesList]);
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPost = capabilitiesList.slice(firstPostIndex, lastPostIndex);
 
   return (
     <div className='bg-white'>
       <div className='mt-6'>
         {capabilitiesList.length ? (
-          <div className='overflow-x-auto rounded-xl border border-slate-300'>
-            <table className='table-auto w-full table'>
-              {/* Table header */}
-              <thead className='text-xs text-textTableHeader font-semibold border-b border-slate-200 bg-slate-50'>
-                <tr>
-                  <th className='px-2 first:pl-5 w-1/4'>
-                    <div className='font-semibold text-left'>
-                      Lista de productos
-                    </div>
-                  </th>
-                  <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-                    <p className='font-semibold text-left'>Tipo</p>
-                  </th>
-                  <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-                    <p className='font-semibold text-left'>Grupo</p>
-                  </th>
-                  <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
-                    <p className='font-semibold text-center'>Pz/Hora</p>
-                  </th>
-                  <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-                    <p className='font-semibold text-center'>Turno/Dia</p>
-                  </th>
-                  <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-                    <p className='font-semibold text-center'>Pz/Dia</p>
-                  </th>
-                  <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-2/5'>
-                    <p className='font-semibold text-center'>Comentario</p>
-                  </th>
-                </tr>
-              </thead>
-              {/* Table body */}
-              <tbody className='text-sm divide-y divide-slate-200'>
-                <CapabilitiesTableItem
-                  setTransactionPanelOpen={setTransactionPanelOpen}
-                  setGroupPanelOpen={setGroupPanelOpen}
-                  capabilitiesList={capabilitiesList}
-                />
-              </tbody>
-            </table>
-          </div>
+          <>
+            <div className='overflow-x-auto rounded-xl border border-slate-300'>
+              <table className='table-auto w-full table'>
+                {/* Table header */}
+                <thead className='text-xs text-textTableHeader font-semibold border-b border-slate-200 bg-slate-50'>
+                  <tr>
+                    <th className='px-2 first:pl-5 w-1/4'>
+                      <div className='font-semibold text-left'>
+                        Lista de productos
+                      </div>
+                    </th>
+                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
+                      <p className='font-semibold text-left'>Tipo</p>
+                    </th>
+                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
+                      <p className='font-semibold text-left'>Grupo</p>
+                    </th>
+                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
+                      <p className='font-semibold text-center'>Pz/Hora</p>
+                    </th>
+                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
+                      <p className='font-semibold text-center'>Turno/Dia</p>
+                    </th>
+                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
+                      <p className='font-semibold text-center'>Pz/Dia</p>
+                    </th>
+                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-2/5'>
+                      <p className='font-semibold text-center'>Comentario</p>
+                    </th>
+                  </tr>
+                </thead>
+                {/* Table body */}
+                <tbody className='text-sm divide-y divide-slate-200'>
+                  <CapabilitiesTableItem
+                    setTransactionPanelOpen={setTransactionPanelOpen}
+                    setGroupPanelOpen={setGroupPanelOpen}
+                    capabilitiesList={currentPost}
+                  />
+                </tbody>
+              </table>
+            </div>
+            <section className='mt-5'>
+              <PaginationCapabilities
+                totalPosts={capabilitiesList.length}
+                postsPerPage={postsPerPage}
+                setCurrentPage={setCurrentPage}
+              />
+            </section>
+          </>
         ) : (
           <>
             <section className='justify-center items-center flex h-96'>
--- src/partials/management/user/UserTable.jsx
+++ src/partials/management/user/UserTable.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import UserTableItem from './UserTableItem';
 import { getAlluser, selectAllUser } from '../../../store/slice/usersSlice';
 import { useDispatch, useSelector } from 'react-redux';
-
+import PaginationUser from '../../../components/PaginationUser';
 function UserTable({
   setUserPanelOpen,
   setOpenModalUserDelete,
   openModalUserDelete,
   userPanelOpen,
 }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(11);
   const dispatch = useDispatch();
   const dataUser = useSelector(selectAllUser);
 
@@ -16,57 +18,60 @@ function UserTable({
     dispatch(getAlluser());
   }, [dataUser]);
 
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPost = dataUser.slice(firstPostIndex, lastPostIndex);
+
   return (
     <>
       {dataUser.length ? (
-        <section>
-          <div className='overflow-x-auto rounded-xl border border-slate-300'>
-            <table className='table-auto w-full'>
-              <thead className='text-xs text-textTableHeader font-semibold border-b border-slate-200 bg-slate-50'>
-                <tr>
-                  <th className='px-2 first:pl-5'>
-                    <div className='font-semibold text-left'>Nombre</div>
-                  </th>
-                  <th className='px-24 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
-                    <p className='font-semibold text-left'>Email</p>
-                  </th>
-                  <th className='px-4 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
-                    <p className='font-semibold text-left'>NMC</p>
-                  </th>
-                  <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
-                    <p className='font-semibold text-center'>Teléfono</p>
-                  </th>
-                  <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-                    <p className='font-semibold text-center'>Posición</p>
-                  </th>
-                  <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-                    <p className='font-semibold text-center'></p>
-                  </th>
-                </tr>
-              </thead>
-              {/* Table body */}
-              <tbody className='text-sm divide-y divide-slate-200'>
-                {dataUser.map((data) => (
+        <>
+          <section>
+            <div className='overflow-x-auto rounded-xl border border-slate-300'>
+              <table className='table-auto w-full'>
+                <thead className='text-xs text-textTableHeader font-semibold border-b border-slate-200 bg-slate-50'>
+                  <tr>
+                    <th className='px-2 first:pl-5'>
+                      <div className='font-semibold text-left'>Nombre</div>
+                    </th>
+                    <th className='px-24 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
+                      <p className='font-semibold text-left'>Email</p>
+                    </th>
+                    <th className='px-4 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
+                      <p className='font-semibold text-left'>NMC</p>
+                    </th>
+                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
+                      <p className='font-semibold text-center'>Teléfono</p>
+                    </th>
+                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
+                      <p className='font-semibold text-center'>Posición</p>
+                    </th>
+                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
+                      <p className='font-semibold text-center'></p>
+                    </th>
+                  </tr>
+                </thead>
+                {/* Table body */}
+                <tbody className='text-sm divide-y divide-slate-200'>
                   <UserTableItem
                     userPanelOpen={userPanelOpen}
                     setUserPanelOpen={setUserPanelOpen}
                     setOpenModalUserDelete={setOpenModalUserDelete}
                     openModalUserDelete={openModalUserDelete}
-                    first_name={data.first_name}
-                    last_name={data.last_name}
-                    email={data.email}
-                    nmc={data.nmc}
-                    position={data.position}
-                    telephone={data.telephone}
-                    key={data.id}
-                    id={data.id}
-                    role={data.role.name}
+                    dataUser={currentPost}
                   />
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </section>
+                </tbody>
+              </table>
+            </div>
+          </section>
+          <section className='mt-5'>
+            <PaginationUser
+              totalPosts={dataUser.length}
+              postsPerPage={postsPerPage}
+              setCurrentPage={setCurrentPage}
+            />
+          </section>
+        </>
       ) : (
         <>
           <div className='justify-center items-center flex h-96'>
