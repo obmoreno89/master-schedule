eeded4d0d95e948bf8f01de42ca21638bd847fe4
Maximiliano Montaña <maxi.omr01@gmail.com>
2022-12-16 17:15:51 -0300

    se agrega funcionalidad de ordenamiento en tabla capacidades

--- src/partials/capabilities/CapabilitiesTable.jsx
+++ src/partials/capabilities/CapabilitiesTable.jsx
@@ -1,63 +1,175 @@
-import React, { useEffect, useState } from 'react';
-import CapabilitiesTableItem from './CapabilitiesTableItem';
-import PaginationCapabilities from '../../components/PaginationCapabilities';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import CapabilitiesTableItem from "./CapabilitiesTableItem";
+import PaginationCapabilities from "../../components/PaginationCapabilities";
+import { useSelector, useDispatch } from "react-redux";
 import {
   selectCapabilitiesList,
   getCapabilitiesList,
-} from '../../store/slice/capabilitiesSlice';
+} from "../../store/slice/capabilitiesSlice";
+import icons from "../../images/icon/icons";
+import { orderGAsc, orderGDesc, orderPLAsc, orderPLDesc } from "./orderFunc";
 
 const CapabilitiesTable = ({ setTransactionPanelOpen, setGroupPanelOpen }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
+  const [capabilities, setCapabilities] = useState(
+    useSelector(selectCapabilitiesList)
+  );
+  const [orderPL, setOrderPL] = useState({ state: false, asc: false });
+  const [orderG, setOrderG] = useState({ state: false, asc: false });
   const capabilitiesList = useSelector(selectCapabilitiesList);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getCapabilitiesList());
+  }, []);
+
+  useEffect(() => {
+    setCapabilities(capabilitiesList);
   }, [capabilitiesList]);
+
+  useEffect(() => {
+    if (orderPL.state) {
+      if (!orderPL.asc) {
+        orderPLAsc(capabilitiesList, setCapabilities);
+      } else {
+        orderPLDesc(capabilitiesList, setCapabilities);
+      }
+    }
+  }, [orderPL]);
+
+  useEffect(() => {
+    if (orderG.state) {
+      if (!orderG.asc) {
+        orderGAsc(capabilitiesList, setCapabilities);
+      } else {
+        orderGDesc(capabilitiesList, setCapabilities);
+      }
+    }
+  }, [orderG]);
 
   const lastPostIndex = currentPage * postsPerPage;
   const firstPostIndex = lastPostIndex - postsPerPage;
-  const currentPost = capabilitiesList.slice(firstPostIndex, lastPostIndex);
+  const currentPost = capabilities.slice(firstPostIndex, lastPostIndex);
+
+  // const orderPLAsc = () => {
+  //   const arrayForSort = [...capabilitiesList];
+  //   const sortArray = arrayForSort.sort((a, b) => {
+  //     if (a.product_line.name < b.product_line.name) {
+  //       return 1;
+  //     }
+  //     if (a.product_line.name > b.product_line.name) {
+  //       return -1;
+  //     }
+  //     return 0;
+  //   });
+  //   setCapabilities(sortArray);
+  // };
+
+  // const orderGAsc = () => {
+  //   const arrayForSort = [...capabilitiesList];
+  //   const sortArray = arrayForSort.sort((a, b) => {
+  //     if (a.product_line.group.name < b.product_line.group.name) {
+  //       return 1;
+  //     }
+  //     if (a.product_line.group.name > b.product_line.group.name) {
+  //       return -1;
+  //     }
+  //     return 0;
+  //   });
+  //   setCapabilities(sortArray);
+  // };
+
+  // const orderPLDesc = () => {
+  //   const arrayForSort = [...capabilitiesList];
+  //   const sortArray = arrayForSort.sort((a, b) => {
+  //     if (a.product_line.name > b.product_line.name) {
+  //       return 1;
+  //     }
+  //     if (a.product_line.name < b.product_line.name) {
+  //       return -1;
+  //     }
+  //     return 0;
+  //   });
+  //   setCapabilities(sortArray);
+  // };
+
+  // const orderGDesc = () => {
+  //   const arrayForSort = [...capabilitiesList];
+  //   const sortArray = arrayForSort.sort((a, b) => {
+  //     if (a.product_line.group.name > b.product_line.group.name) {
+  //       return 1;
+  //     }
+  //     if (a.product_line.group.name < b.product_line.group.name) {
+  //       return -1;
+  //     }
+  //     return 0;
+  //   });
+
+  //   setCapabilities(sortArray);
+  // };
 
   return (
-    <div className='bg-white'>
-      <div className='mt-6'>
-        {capabilitiesList.length ? (
+    <div className="bg-white">
+      <div className="mt-6">
+        {capabilities?.length ? (
           <>
-            <div className='overflow-x-auto rounded-xl border border-slate-300'>
-              <table className='table-auto w-full table'>
+            <div className="overflow-x-auto rounded-xl border border-slate-300">
+              <table className="table-auto w-full table">
                 {/* Table header */}
-                <thead className='text-xs text-textTableHeader font-semibold border-b border-slate-200 bg-slate-50'>
+                <thead className="text-xs text-textTableHeader font-semibold border-b border-slate-200 bg-slate-50">
                   <tr>
-                    <th className='px-2 first:pl-5 w-1/4'>
-                      <div className='font-semibold text-left'>
-                        Línea de productos
+                    <th
+                      className="px-2 first:pl-5 w-1/4 cursor-pointer"
+                      onClick={() => {
+                        setOrderPL({ state: true, asc: !orderPL.asc });
+                      }}
+                    >
+                      <div className="flex items-center space-x-10">
+                        <div className="font-semibold text-left">
+                          Línea de productos
+                        </div>
+                        <img
+                          src={orderPL.asc ? icons.doubleDown : icons.doubleUp}
+                          alt="Flecha abajo"
+                          className="w-5"
+                        />
                       </div>
                     </th>
-                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-                      <p className='font-semibold text-left'>Tipo</p>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap ">
+                      <p className="font-semibold text-left">Tipo</p>
                     </th>
-                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-                      <p className='font-semibold text-left'>Grupo</p>
+                    <th
+                      className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap cursor-pointer "
+                      onClick={() => {
+                        setOrderG({ state: true, asc: !orderG.asc });
+                      }}
+                    >
+                      <div className="flex items-center space-x-2">
+                        <p className="font-semibold text-left">Grupo</p>
+                        <img
+                          src={orderG.asc ? icons.doubleDown : icons.doubleUp}
+                          alt="Flecha abajo"
+                          className="w-5"
+                        />
+                      </div>
                     </th>
-                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
-                      <p className='font-semibold text-center'>Pz/Hora</p>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <p className="font-semibold text-center">Pz/Hora</p>
                     </th>
-                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-                      <p className='font-semibold text-center'>Turno/Dia</p>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap ">
+                      <p className="font-semibold text-center">Turno/Dia</p>
                     </th>
-                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-                      <p className='font-semibold text-center'>Pz/Dia</p>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap ">
+                      <p className="font-semibold text-center">Pz/Dia</p>
                     </th>
-                    <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-2/5'>
-                      <p className='font-semibold text-center'>Comentario</p>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-2/5">
+                      <p className="font-semibold text-center">Comentario</p>
                     </th>
                   </tr>
                 </thead>
                 {/* Table body */}
-                <tbody className='text-sm divide-y divide-slate-200'>
+                <tbody className="text-sm divide-y divide-slate-200">
                   <CapabilitiesTableItem
                     setTransactionPanelOpen={setTransactionPanelOpen}
                     setGroupPanelOpen={setGroupPanelOpen}
@@ -66,7 +178,7 @@ const CapabilitiesTable = ({ setTransactionPanelOpen, setGroupPanelOpen }) => {
                 </tbody>
               </table>
             </div>
-            <section className='mt-5'>
+            <section className="mt-5">
               <PaginationCapabilities
                 totalPosts={capabilitiesList.length}
                 postsPerPage={postsPerPage}
@@ -76,8 +188,8 @@ const CapabilitiesTable = ({ setTransactionPanelOpen, setGroupPanelOpen }) => {
           </>
         ) : (
           <>
-            <section className='justify-center items-center flex h-96'>
-              <h2 className='font-semibold text-2xl'>Sin datos que mostrar</h2>
+            <section className="justify-center items-center flex h-96">
+              <h2 className="font-semibold text-2xl">Sin datos que mostrar</h2>
             </section>
           </>
         )}
