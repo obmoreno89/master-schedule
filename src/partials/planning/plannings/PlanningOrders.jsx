240a837142fcd7baa33693017523fd148c7f4602
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2022-12-26 16:27:44 -0600

    se arreglo bug

--- src/partials/planning/plannings/PlanningOrders.jsx
+++ src/partials/planning/plannings/PlanningOrders.jsx
@@ -32,11 +32,7 @@ const PlanningOrders = ({}) => {
 
   useEffect(() => {
     if (groups.length === 0) {
-
-
-      navigate("/mp-pro/planning/plannings/");
-
-
+      navigate('/mp-pro/planning/plannings/');
     }
   }, [groups]);
 
@@ -46,70 +42,70 @@ const PlanningOrders = ({}) => {
       nameRoute={'Planeación'}
       nameSubRoute={'Planeaciones'}
     >
+      <div>
+        <section>
+          <header className='flex flex-1 py-5 justify-between'>
+            <h2 className='text-3xl font-semibold text-black my-auto'>
+              Órdenes a planear
+            </h2>
+            {!notFound && orders?.length > 0 && (
+              <p className='my-auto mb-1 font-medium'>
+                Total de órdenes:{' '}
+                <span className='font-bold text-primary'>{orders?.length}</span>
+              </p>
+            )}
+          </header>
+          <main>
+            {notFound ? (
+              <section className='justify-center items-center flex orders-table'>
+                <h2 className='font-semibold text-2xl'>
+                  Sin datos para mostrar
+                </h2>
+              </section>
+            ) : orders?.length > 0 ? (
+              <PlanningOrdersTable orders={orders} />
+            ) : (
+              // <div className="flex justify-center py-5">
+              <section className='justify-center items-center flex orders-table'>
+                <div className='loader'></div>
+                <span className='ml-3 text-primary font-semibold'>
+                  Cargando
+                </span>
+              </section>
+            )}
 
-      <section>
-        <header className='flex flex-1 py-5 justify-between'>
-          <h2 className='text-3xl font-semibold text-black my-auto'>
+            <div className='flex justify-end py-5'>
+              <Link to='/mp-pro/planning/plannings/'>
+                <button className='border border-slate-300 rounded w-64 h-12 text-base font-semibold mr-6'>
+                  Cancelar
+                </button>
+              </Link>
 
-            Órdenes a planear
-          </h2>
-          {!notFound && orders?.length > 0 && (
-            <p className='my-auto mb-1 font-medium'>
-              Total de órdenes:{' '}
-              <span className='font-bold text-primary'>{orders?.length}</span>
-            </p>
-          )}
-        </header>
-        <main>
-          {notFound ? (
-            <section className='justify-center items-center flex orders-table'>
-              <h2 className='font-semibold text-2xl'>Sin datos para mostrar</h2>
-            </section>
-          ) : orders?.length > 0 ? (
-            <PlanningOrdersTable orders={orders} />
-          ) : (
-            // <div className="flex justify-center py-5">
-            <section className='justify-center items-center flex orders-table'>
-              <div className='loader'></div>
-              <span className='ml-3 text-primary font-semibold'>Cargando</span>
-            </section>
-          )}
+              <button
+                onClick={() => setOrdersPanelOpen(true)}
+                className={`w-80 h-12 bg-primary rounded text-white text-base flex justify-center hover:bg-secondary hover:text-primary ${
+                  notFound && 'cursor-not-allowed'
+                }`}
+                disabled={notFound ? true : false}
+              >
+                <span className='my-auto'>Continuar</span>
 
-
-          <div className="flex justify-end py-5">
-            <Link to="/mp-pro/planning/plannings/">
-              <button className="border border-slate-300 rounded w-64 h-12 text-base font-semibold mr-6">
-
-
-                Cancelar
+                <img
+                  src={icons.arrowRight}
+                  alt='icon-arrow-right'
+                  className='my-auto ml-3 text-white'
+                />
               </button>
-            </Link>
+            </div>
 
-            <button
-              onClick={() => setOrdersPanelOpen(true)}
-              className={`w-80 h-12 bg-primary rounded text-white text-base flex justify-center hover:bg-secondary hover:text-primary ${
-                notFound && 'cursor-not-allowed'
-              }`}
-              disabled={notFound ? true : false}
-            >
-              <span className='my-auto'>Continuar</span>
-
-              <img
-                src={icons.arrowRight}
-                alt='icon-arrow-right'
-                className='my-auto ml-3 text-white'
+            <div>
+              <PlanningOrdersPanel
+                ordersPanelOpen={ordersPanelOpen}
+                setOrdersPanelOpen={setOrdersPanelOpen}
               />
-            </button>
-          </div>
-
-          <div>
-            <PlanningOrdersPanel
-              ordersPanelOpen={ordersPanelOpen}
-              setOrdersPanelOpen={setOrdersPanelOpen}
-            />
-          </div>
-        </main>
-      </section>
+            </div>
+          </main>
+        </section>
       </div>
     </Layout>
   );
