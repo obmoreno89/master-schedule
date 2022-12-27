9cf469fad270e5b97d493491fdb85f9a22b3483e
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2022-12-27 09:22:39 -0600

    esqueleto del panel para crear capacidad

--- src/partials/capabilities/CapabilitiesTable.jsx
+++ src/partials/capabilities/CapabilitiesTable.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import CapabilitiesTableItem from './CapabilitiesTableItem';
+import CapabilitiesPanel from './CapabilitiesPanel';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   selectReload,
@@ -10,7 +11,12 @@ import {
   selectCapabilitiesSearch,
 } from '../../store/slice/capabilitiesSlice';
 
-const CapabilitiesTable = ({ setTransactionPanelOpen, setGroupPanelOpen }) => {
+const CapabilitiesTable = ({
+  setTransactionPanelOpen,
+  setGroupPanelOpen,
+  setCapabilitiesOpenPanel,
+  capabilitiesPanelOpen,
+}) => {
   const dispatch = useDispatch();
   const [capabilities, setCapabilities] = useState(
     useSelector(selectCapabilitiesList)
@@ -55,6 +61,12 @@ const CapabilitiesTable = ({ setTransactionPanelOpen, setGroupPanelOpen }) => {
 
   return (
     <div className='bg-white'>
+      <section>
+        <CapabilitiesPanel
+          setCapabilitiesOpenPanel={setCapabilitiesOpenPanel}
+          capabilitiesPanelOpen={capabilitiesPanelOpen}
+        />
+      </section>
       <div className='mt-6'>
         <section className='flex justify-between mb-5'>
           <div className='mb-4 sm:mb-0'>
@@ -62,12 +74,30 @@ const CapabilitiesTable = ({ setTransactionPanelOpen, setGroupPanelOpen }) => {
               Capacidades
             </h1>
           </div>
-          <input
-            className='form-input w-72'
-            placeholder='Buscar...'
-            type='search'
-            onChange={handleSearch}
-          />
+          <div className='flex space-x-3'>
+            <input
+              className='form-input w-72'
+              placeholder='Buscar...'
+              type='search'
+              onChange={handleSearch}
+            />
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                setCapabilitiesOpenPanel(true);
+              }}
+              type='button'
+              className='btn bg-primary text-white w-54 space-x-2'
+            >
+              <svg
+                className='w-4 h-4 fill-current opacity-50 shrink-0'
+                viewBox='0 0 16 16'
+              >
+                <path d='M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z' />
+              </svg>
+              <span>Crear capacidad</span>
+            </button>
+          </div>
         </section>
         {capabilities?.length > 0 ? (
           <>
