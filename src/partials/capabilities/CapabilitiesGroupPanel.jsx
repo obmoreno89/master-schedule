df729810373f882488a5cc607b77ab87597d54e4
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2022-12-28 14:05:31 -0600

    cambios en los titulos de los paneles

--- src/partials/capabilities/CapabilitiesGroupPanel.jsx
+++ src/partials/capabilities/CapabilitiesGroupPanel.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from "react";
-import Transition from "../../utils/Transition";
-import CapabilitiesGroupPanelTable from "./CapabilitiesGroupPanelTable";
+import React, { useEffect, useRef } from 'react';
+import Transition from '../../utils/Transition';
+import CapabilitiesGroupPanelTable from './CapabilitiesGroupPanelTable';
 
 function CapabilitiesGroupPanel({
   setGroupPanelOpen,
@@ -33,57 +33,57 @@ function CapabilitiesGroupPanel({
       if (!groupPanelOpen || keyCode !== 27) return;
       setGroupPanelOpen(false);
     };
-    document.addEventListener("keydown", keyHandler);
-    return () => document.removeEventListener("keydown", keyHandler);
+    document.addEventListener('keydown', keyHandler);
+    return () => document.removeEventListener('keydown', keyHandler);
   });
 
   return (
     <>
       <Transition
-        className="fixed inset-0 bg-slate-900 bg-opacity-30 z-50 transition-opacity"
+        className='fixed inset-0 bg-slate-900 bg-opacity-30 z-50 transition-opacity'
         show={groupPanelOpen}
-        enter="transition ease-out duration-200"
-        enterStart="opacity-0"
-        enterEnd="opacity-100"
-        leave="transition ease-out duration-200"
-        leaveStart="opacity-100"
-        leaveEnd="opacity-0"
-        aria-hidden="true"
+        enter='transition ease-out duration-200'
+        enterStart='opacity-0'
+        enterEnd='opacity-100'
+        leave='transition ease-out duration-200'
+        leaveStart='opacity-100'
+        leaveEnd='opacity-0'
+        aria-hidden='true'
       />
       <Transition
-        id="panelG"
-        className="fixed inset-0 z-50 overflow-hidden flex items-center justify-center transform px-4 sm:px-6"
-        role="dialog"
-        aria-modal="true"
+        id='panelG'
+        className='fixed inset-0 z-50 overflow-hidden flex items-center justify-center transform px-4 sm:px-6'
+        role='dialog'
+        aria-modal='true'
         show={groupPanelOpen}
-        enter="transition ease-in-out duration-500"
-        enterStart="opacity-0 translate-x-4"
-        enterEnd="opacity-100 translate-x-0"
-        leave="transition ease-in-out duration-500"
-        leaveStart="opacity-100 translate-x-0"
-        leaveEnd="opacity-0 translate-x-4"
+        enter='transition ease-in-out duration-500'
+        enterStart='opacity-0 translate-x-4'
+        enterEnd='opacity-100 translate-x-0'
+        leave='transition ease-in-out duration-500'
+        leaveStart='opacity-100 translate-x-0'
+        leaveEnd='opacity-0 translate-x-4'
       >
         <div
           ref={panelContent}
           className={`absolute inset-0 sm:left-auto z-40 transform shadow-xl transition-transform duration-200 ease-in-out ${
-            groupPanelOpen ? "translate-x-" : "translate-x-full"
+            groupPanelOpen ? 'translate-x-' : 'translate-x-full'
           }`}
         >
-          <div className="top-16 bg-white overflow-x-hidden overflow-y-auto no-scrollbar shrink-0 border-l border-slate-200 w-full sm:w-[390px] h-screen">
-            <h2 className="mt-4 ml-4 w-48 font-bold text-black">
-              Seleccione un grupo
+          <div className='top-16 bg-white overflow-x-hidden overflow-y-auto no-scrollbar shrink-0 border-l border-slate-200 w-full sm:w-[390px] h-screen'>
+            <h2 className='mt-4 ml-4 w-full font-bold text-black text-2xl'>
+              Grupos
             </h2>
             <button
               ref={closeBtn}
               onClick={() => setGroupPanelOpen(false)}
-              className="absolute top-0 right-0 mt-4 mr-3 group p-1"
+              className='absolute top-0 right-0 mt-4 mr-3 group p-1'
             >
               <svg
-                className="w-4 h-4 fill-slate-400 group-hover:fill-slate-600 pointer-events-none"
-                viewBox="0 0 16 16"
-                xmlns="http://www.w3.org/2000/svg"
+                className='w-5 h-5 fill-slate-800 group-hover:fill-slate-600 pointer-events-none'
+                viewBox='0 0 16 16'
+                xmlns='http://www.w3.org/2000/svg'
               >
-                <path d="m7.95 6.536 4.242-4.243a1 1 0 1 1 1.415 1.414L9.364 7.95l4.243 4.242a1 1 0 1 1-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 0 1-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 0 1 1.414-1.414L7.95 6.536Z" />
+                <path d='m7.95 6.536 4.242-4.243a1 1 0 1 1 1.415 1.414L9.364 7.95l4.243 4.242a1 1 0 1 1-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 0 1-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 0 1 1.414-1.414L7.95 6.536Z' />
               </svg>
             </button>
             <CapabilitiesGroupPanelTable
