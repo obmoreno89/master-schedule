14d0479c3b1a1f2f25f5bc1daa30d1f1604b82e5
Maximiliano Montaña <maxi.omr01@gmail.com>
2023-01-10 18:37:44 -0300

    se modifica objeto que se envia al servidor para obtener ordenes a planear

--- src/partials/planning/plannings/PlanningOrders.jsx
+++ src/partials/planning/plannings/PlanningOrders.jsx
@@ -1,18 +1,19 @@
-import Layout from '../../../components/Layout';
-import icons from '../../../images/icon/icons';
-import { useState } from 'react';
-import PlanningOrdersTable from './PlanningOrdersTable';
-import PlanningsCapabilitiesPanel from './PlanningsCapabilitiesPanel';
-import { Link, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import Layout from "../../../components/Layout";
+import icons from "../../../images/icon/icons";
+import { useState } from "react";
+import PlanningOrdersTable from "./PlanningOrdersTable";
+import PlanningsCapabilitiesPanel from "./PlanningsCapabilitiesPanel";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getOrders,
   selectGroups,
   selectNotFound,
   selectOrders,
-} from '../../../store/slice/planningSlice';
-import Loading from '../../../pages/component/Loading';
+  selectPlanning,
+} from "../../../store/slice/planningSlice";
+import Loading from "../../../pages/component/Loading";
 
 const PlanningOrders = () => {
   const navigate = useNavigate();
@@ -22,45 +23,52 @@ const PlanningOrders = () => {
   const orders = useSelector(selectOrders);
   const groups = useSelector(selectGroups);
   const notFound = useSelector(selectNotFound);
+  const planning = useSelector(selectPlanning);
 
   useEffect(() => {
-    const data = {
-      group: groups.toString(),
-      // Aqui van a ir los IDs de los criterios a aplicar.
-      criteria: [2, 3, 5],
+    const json = {
+      group: planning?.group,
+      criteria: [
+        planning?.criteria[2],
+        planning?.criteria[3],
+        planning?.criteria[4],
+      ],
     };
-    dispatch(getOrders(data));
+    //dispatch(getOrders(planning));
+    console.log(planning);
+    //console.log(json);
+    dispatch(getOrders(json));
   }, [groups]);
 
   useEffect(() => {
     if (groups.length === 0) {
-      navigate('/mp-pro/planning/plannings/');
+      navigate("/mp-pro/planning/plannings/");
     }
   }, [groups]);
 
   return (
     <Layout
       icon={icons.planningIcon}
-      nameRoute={'Planeación'}
-      nameSubRoute={'Ordenes'}
+      nameRoute={"Planeación"}
+      nameSubRoute={"Ordenes"}
     >
-      <div className='px-4 sm:px-6 lg:px-0 py-1 w-full max-w-9xl mx-auto'>
-        <section className='lg:px-8'>
-          <header className='flex flex-1 py-5 justify-between'>
-            <h2 className='text-3xl font-semibold text-black my-auto'>
+      <div className="px-4 sm:px-6 lg:px-0 py-1 w-full max-w-9xl mx-auto">
+        <section className="lg:px-8">
+          <header className="flex flex-1 py-5 justify-between">
+            <h2 className="text-3xl font-semibold text-black my-auto">
               Órdenes a planear
             </h2>
             {!notFound && orders?.length > 0 && (
-              <p className='my-auto mb-1 font-medium'>
-                Total de órdenes:{' '}
-                <span className='font-bold text-primary'>{orders?.length}</span>
+              <p className="my-auto mb-1 font-medium">
+                Total de órdenes:{" "}
+                <span className="font-bold text-primary">{orders?.length}</span>
               </p>
             )}
           </header>
           <main>
             {notFound ? (
-              <section className='justify-center items-center flex orders-table'>
-                <h2 className='font-semibold text-2xl'>
+              <section className="justify-center items-center flex orders-table">
+                <h2 className="font-semibold text-2xl">
                   Sin datos para mostrar
                 </h2>
               </section>
@@ -70,9 +78,9 @@ const PlanningOrders = () => {
               <Loading />
             )}
 
-            <div className='flex justify-end py-5'>
-              <Link to='/mp-pro/planning/plannings/'>
-                <button className='border border-slate-300 rounded w-64 h-12 text-base font-semibold mr-6'>
+            <div className="flex justify-end py-5">
+              <Link to="/mp-pro/planning/plannings/">
+                <button className="border border-slate-300 rounded w-64 h-12 text-base font-semibold mr-6">
                   Cancelar
                 </button>
               </Link>
@@ -82,16 +90,16 @@ const PlanningOrders = () => {
                   setPlanningCapabilities(true);
                 }}
                 className={`w-80 h-12 bg-primary rounded text-white text-base flex justify-center hover:bg-secondary hover:text-primary ${
-                  (notFound || orders?.length === 0) && 'cursor-not-allowed'
+                  (notFound || orders?.length === 0) && "cursor-not-allowed"
                 }`}
                 disabled={notFound || orders?.length === 0 ? true : false}
               >
-                <span className='my-auto'>Continuar</span>
+                <span className="my-auto">Continuar</span>
 
                 <img
                   src={icons.arrowRight}
-                  alt='icon-arrow-right'
-                  className='my-auto ml-3 text-white'
+                  alt="icon-arrow-right"
+                  className="my-auto ml-3 text-white"
                 />
               </button>
             </div>
--- src/partials/planning/plannings/PlanningsCapabilitiesPanel.jsx
+++ src/partials/planning/plannings/PlanningsCapabilitiesPanel.jsx
@@ -1,12 +1,13 @@
-import { useEffect, useRef, useState } from 'react';
-import Transition from '../../../utils/Transition';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef, useState } from "react";
+import Transition from "../../../utils/Transition";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import {
   generateGantt,
   selectGanttLoading,
-} from '../../../store/slice/planningSlice';
-import ButtonLoading from '../../../helpers/ButtonLoading';
+  selectPlanning,
+} from "../../../store/slice/planningSlice";
+import ButtonLoading from "../../../helpers/ButtonLoading";
 
 const PlanningsCapabilitiesPanel = ({
   orders,
@@ -19,14 +20,16 @@ const PlanningsCapabilitiesPanel = ({
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const loading = useSelector(selectGanttLoading);
+  // const planning = useSelector(selectPlanning);
+  // console.log(planning);
 
   const [byDefault, setByDefault] = useState({
     state: true,
-    value: 'Capacidad base (Por defecto)',
+    value: "Capacidad base (Por defecto)",
   });
   const [advanced, setAdvanced] = useState({
     state: false,
-    value: 'Capacidad Personalizada (Avanzado)',
+    value: "Capacidad Personalizada (Avanzado)",
   });
 
   // close if the esc key is pressed
@@ -35,8 +38,8 @@ const PlanningsCapabilitiesPanel = ({
       if (!planningCapabilities || keyCode !== 27) return;
       setPlanningCapabilities(false);
     };
-    document.addEventListener('keydown', keyHandler);
-    return () => document.removeEventListener('keydown', keyHandler);
+    document.addEventListener("keydown", keyHandler);
+    return () => document.removeEventListener("keydown", keyHandler);
   });
 
   const handleCheckbox = (e) => {
@@ -47,7 +50,7 @@ const PlanningsCapabilitiesPanel = ({
     const data = {
       orders: orders,
       selected_groups: groups,
-      criteria: ['A'],
+      criteria: ["A"],
     };
     dispatch(generateGantt(data, navigate));
   };
@@ -55,83 +58,83 @@ const PlanningsCapabilitiesPanel = ({
   return (
     <>
       <Transition
-        className='fixed inset-0 bg-slate-900 bg-opacity-30 z-50 transition-opacity'
+        className="fixed inset-0 bg-slate-900 bg-opacity-30 z-50 transition-opacity"
         show={planningCapabilities}
-        enter='transition ease-out duration-200'
-        enterStart='opacity-0'
-        enterEnd='opacity-100'
-        leave='transition ease-out duration-200'
-        leaveStart='opacity-100'
-        leaveEnd='opacity-0'
-        aria-hidden='true'
+        enter="transition ease-out duration-200"
+        enterStart="opacity-0"
+        enterEnd="opacity-100"
+        leave="transition ease-out duration-200"
+        leaveStart="opacity-100"
+        leaveEnd="opacity-0"
+        aria-hidden="true"
       />
       <Transition
-        id='panelG'
-        className='fixed inset-0 z-50 overflow-hidden flex items-center justify-center transform px-4 sm:px-6'
-        role='dialog'
-        aria-modal='true'
+        id="panelG"
+        className="fixed inset-0 z-50 overflow-hidden flex items-center justify-center transform px-4 sm:px-6"
+        role="dialog"
+        aria-modal="true"
         show={planningCapabilities}
-        enter='transition ease-in-out duration-500'
-        enterStart='opacity-0 translate-x-4'
-        enterEnd='opacity-100 translate-x-0'
-        leave='transition ease-in-out duration-500'
-        leaveStart='opacity-100 translate-x-0'
-        leaveEnd='opacity-0 translate-x-4'
+        enter="transition ease-in-out duration-500"
+        enterStart="opacity-0 translate-x-4"
+        enterEnd="opacity-100 translate-x-0"
+        leave="transition ease-in-out duration-500"
+        leaveStart="opacity-100 translate-x-0"
+        leaveEnd="opacity-0 translate-x-4"
       >
         <div
           ref={panelContent}
           className={`w-[480px] bg-white absolute inset-0 sm:left-auto z-40 transform shadow-xl transition-transform duration-200 ease-in-out ${
-            planningCapabilities ? 'translate-x-' : 'translate-x-full'
+            planningCapabilities ? "translate-x-" : "translate-x-full"
           }`}
         >
-          <section className='mb-10'>
-            <h2 className='mt-4 ml-5 w-full font-bold text-black text-2xl'>
+          <section className="mb-10">
+            <h2 className="mt-4 ml-5 w-full font-bold text-black text-2xl">
               Capacidades
             </h2>
             <button
               ref={closeBtn}
               onClick={() => setPlanningCapabilities(false)}
-              className='absolute top-1 right-0 mt-4 mr-3 group p-1'
+              className="absolute top-1 right-0 mt-4 mr-3 group p-1"
             >
               <svg
-                className='w-5 h-5 fill-slate-800 group-hover:fill-slate-600 pointer-events-none'
-                viewBox='0 0 16 16'
-                xmlns='http://www.w3.org/2000/svg'
+                className="w-5 h-5 fill-slate-800 group-hover:fill-slate-600 pointer-events-none"
+                viewBox="0 0 16 16"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <path d='m7.95 6.536 4.242-4.243a1 1 0 1 1 1.415 1.414L9.364 7.95l4.243 4.242a1 1 0 1 1-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 0 1-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 0 1 1.414-1.414L7.95 6.536Z' />
+                <path d="m7.95 6.536 4.242-4.243a1 1 0 1 1 1.415 1.414L9.364 7.95l4.243 4.242a1 1 0 1 1-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 0 1-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 0 1 1.414-1.414L7.95 6.536Z" />
               </svg>
             </button>
           </section>
-          <section className='flex flex-col justify-center px-5'>
-            <div className='mt-5 border border-borderInput h-14 items-center flex px-3 rounded'>
-              <label className='flex items-center'>
+          <section className="flex flex-col justify-center px-5">
+            <div className="mt-5 border border-borderInput h-14 items-center flex px-3 rounded">
+              <label className="flex items-center">
                 <input
-                  type='checkbox'
-                  className='form-checkbox'
-                  name='byDefault'
+                  type="checkbox"
+                  className="form-checkbox"
+                  name="byDefault"
                   defaultChecked={byDefault.state}
                   onChange={handleCheckbox}
                 />
-                <span className='text-base font-semibold ml-2 text-black'>
+                <span className="text-base font-semibold ml-2 text-black">
                   Capacidad base (Por defecto)
                 </span>
               </label>
             </div>
-            <div className='mt-5 mb-20 border border-borderInput h-14 items-center flex px-3 rounded'>
-              <label className='flex items-center'>
+            <div className="mt-5 mb-20 border border-borderInput h-14 items-center flex px-3 rounded">
+              <label className="flex items-center">
                 <input
-                  type='checkbox'
-                  className='form-checkbox'
-                  name='advanced'
+                  type="checkbox"
+                  className="form-checkbox"
+                  name="advanced"
                   defaultChecked={advanced.state}
                   onChange={handleCheckbox}
                 />
-                <span className='text-base font-semibold ml-2 text-black'>
+                <span className="text-base font-semibold ml-2 text-black">
                   Capacidad Personalizada (Avanzado)
                 </span>
               </label>
             </div>
-            <div className='flex justify-center'>
+            <div className="flex justify-center">
               {!loading ? (
                 <button
                   onClick={() => {
@@ -139,13 +142,13 @@ const PlanningsCapabilitiesPanel = ({
                     //setOrdersPanelOpen(true);
                     goOrdersPlanningGantt();
                   }}
-                  className='w-80 h-12 bg-primary rounded text-white text-base flex justify-center items-center hover:bg-secondary hover:text-primary'
+                  className="w-80 h-12 bg-primary rounded text-white text-base flex justify-center items-center hover:bg-secondary hover:text-primary"
                 >
                   Ir a la planeación de órdenes
                 </button>
               ) : (
                 <ButtonLoading
-                  loading='Cargando, puede demorar unos segundos'
+                  loading="Cargando, puede demorar unos segundos"
                   gantt={true}
                 />
               )}
