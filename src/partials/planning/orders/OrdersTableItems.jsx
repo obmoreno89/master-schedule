2ed8a6ce823a54c708a9d5635646ddf4f3bbaa82
Maximiliano Montaña <maxi.omr01@gmail.com>
2023-02-03 19:11:11 -0300

    change planning and open orders components, add filter options and functionality and search inputs

--- src/partials/planning/orders/OrdersTableItems.jsx
+++ src/partials/planning/orders/OrdersTableItems.jsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react';
-import icons from '../../../images/icon/icons';
-import { orderNumAsc, orderNumDesc } from '../../capabilities/orderFunc';
-import { useSelector } from 'react-redux';
-import { selectDataFiltered } from '../../../store/slice/ordersPlannedSlice';
+import { useState, useEffect } from "react";
+import icons from "../../../images/icon/icons";
+import { orderNumAsc, orderNumDesc } from "../../capabilities/orderFunc";
+import { useSelector } from "react-redux";
+import {
+  selectDataFiltered,
+} from "../../../store/slice/ordersPlannedSlice";
 
 function OrdersTableItems({ data, ordersList, setList }) {
   const [orderMax, setOrderMax] = useState({ state: false, asc: false });
@@ -16,9 +18,9 @@ function OrdersTableItems({ data, ordersList, setList }) {
   useEffect(() => {
     if (orderMax.state) {
       if (!orderMax.asc) {
-        orderNumAsc(ordersList, setList, 'maximum_quantity');
+        orderNumAsc(ordersList, setList, "maximum_quantity");
       } else {
-        orderNumDesc(ordersList, setList, 'maximum_quantity');
+        orderNumDesc(ordersList, setList, "maximum_quantity");
       }
     }
   }, [orderMax]);
@@ -26,15 +28,15 @@ function OrdersTableItems({ data, ordersList, setList }) {
   useEffect(() => {
     if (orderMin.state) {
       if (!orderMin.asc) {
-        orderNumAsc(ordersList, setList, 'minimum_quantity');
+        orderNumAsc(ordersList, setList, "minimum_quantity");
       } else {
-        orderNumDesc(ordersList, setList, 'minimum_quantity');
+        orderNumDesc(ordersList, setList, "minimum_quantity");
       }
     }
   }, [orderMin]);
 
   const formatNumber = (num) => {
-    if (num === null || num === '0.0') {
+    if (num === null || num === "0.0") {
       return 0;
     } else {
       return parseInt(num);
@@ -43,97 +45,97 @@ function OrdersTableItems({ data, ordersList, setList }) {
 
   return (
     <>
-      <table className='table-auto w-full table'>
-        <thead className='text-xs text-textTableHeader font-semibold border-b border-slate-200 bg-slate-50'>
+      <table className="table-auto w-full table">
+        <thead className="text-xs text-textTableHeader font-semibold border-b border-slate-200 bg-slate-50">
           <tr>
-            <th className='first:pl-5 cursor-pointer'>
-              <p className='font-semibold text-left'>Item - Categoria</p>
+            <th className="first:pl-5 cursor-pointer">
+              <p className="font-semibold text-left">Item - Categoria</p>
             </th>
-            <th className='py-3 whitespace-nowrap'>
-              <p className='font-semibold text-center'>Organización</p>
+            <th className="py-3 whitespace-nowrap">
+              <p className="font-semibold text-center">Organización</p>
             </th>
             <th
-              className='py-3 whitespace-nowrap cursor-pointer'
+              className="py-3 whitespace-nowrap cursor-pointer"
               onClick={() => {
                 setOrderMin({ state: true, asc: !orderMin.asc });
               }}
             >
-              <div className='flex items-center space-x-2 justify-center '>
-                <div className='font-semibold '>Mínimo</div>
+              <div className="flex items-center space-x-2 justify-center ">
+                <div className="font-semibold ">Mínimo</div>
                 <img
                   src={orderMin.asc ? icons.doubleDown : icons.doubleUp}
-                  alt='Flecha abajo'
-                  className='w-5'
+                  alt="Flecha abajo"
+                  className="w-5"
                 />
               </div>
             </th>
             <th
-              className='py-3 whitespace-nowrap'
+              className="py-3 whitespace-nowrap"
               onClick={() => {
                 setOrderMax({ state: true, asc: !orderMax.asc });
               }}
             >
-              <div className='flex items-center space-x-2 justify-center'>
-                <div className='font-semibold'>Máximo</div>
+              <div className="flex items-center space-x-2 justify-center">
+                <div className="font-semibold">Máximo</div>
                 <img
                   src={orderMax.asc ? icons.doubleDown : icons.doubleUp}
-                  alt='Flecha abajo'
-                  className='w-5'
+                  alt="Flecha abajo"
+                  className="w-5"
                 />
               </div>
             </th>
-            <th className='py-3 whitespace-nowrap'>
-              <p className='font-semibold text-center'>On-Hand</p>
+            <th className="py-3 whitespace-nowrap">
+              <p className="font-semibold text-center">On-Hand</p>
             </th>
-            <th className='py-3 whitespace-nowrap'>
-              <p className='font-semibold text-center'>Re-Order QTY</p>
+            <th className="py-3 whitespace-nowrap">
+              <p className="font-semibold text-center">Re-Order QTY</p>
             </th>
 
-            <th className='py-3 whitespace-nowrap px-2'>
-              <p className='font-semibold text-center'>Progreso</p>
+            <th className="py-3 whitespace-nowrap px-2">
+              <p className="font-semibold text-center">Progreso</p>
             </th>
           </tr>
         </thead>
         {!dataFilter.length > 0 ? (
-          <tbody className='text-sm divide-y divide-slate-200'>
+          <tbody className="text-sm divide-y divide-slate-200">
             {data?.map((item, index) => (
               <tr key={index}>
-                <td className='px-2 first:pl-5 py-3 whitespace-nowrap md:w-1/6'>
-                  <p className='text-textTableItem font-medium capitalize'>
+                <td className="px-2 first:pl-5 py-3 whitespace-nowrap md:w-1/6">
+                  <p className="text-textTableItem font-medium capitalize">
                     {item?.item}
                   </p>
-                  <span className='text-primary'>{item?.category}</span>
+                  <span className="text-primary">{item?.category}</span>
                 </td>
-                <td className='py-3 whitespace-nowrap'>
-                  <p className='text-center'>{item?.org}</p>
+                <td className="py-3 whitespace-nowrap">
+                  <p className="text-center">{item?.org}</p>
                 </td>
-                <td className='py-3 whitespace-nowrap md:w-1/6'>
-                  <p className='font-medium capitalize text-center'>
+                <td className="py-3 whitespace-nowrap md:w-1/6">
+                  <p className="font-medium capitalize text-center">
                     {formatNumber(item?.minimum_quantity)}
                   </p>
                 </td>
-                <td className='py-3 whitespace-nowrap md:w-1/6'>
-                  <p className='font-medium text-center'>
+                <td className="py-3 whitespace-nowrap md:w-1/6">
+                  <p className="font-medium text-center">
                     {formatNumber(item?.maximum_quantity)}
                   </p>
                 </td>
-                <td className='py-3 whitespace-nowrap md:w-1/6'>
-                  <p className='text-center'>
+                <td className="py-3 whitespace-nowrap md:w-1/6">
+                  <p className="text-center">
                     {formatNumber(item?.on_hand_quantity)}
                   </p>
                 </td>
-                <td className='py-3 whitespace-nowrap md:w-1/6'>
-                  <p className='text-center'>
+                <td className="py-3 whitespace-nowrap md:w-1/6">
+                  <p className="text-center">
                     {formatNumber(item?.reorder_quantity)}
                   </p>
                 </td>
 
-                <td className='py-3 whitespace-nowrap px-2'>
-                  <div className='mt-2 h-4 relative rounded-full overflow-hidden border border-slate-300'>
-                    <div className='w-full h-full bg-gray-200 absolute '></div>
+                <td className="py-3 whitespace-nowrap px-2">
+                  <div className="mt-2 h-4 relative rounded-full overflow-hidden border border-slate-300">
+                    <div className="w-full h-full bg-gray-200 absolute "></div>
                     <div
-                      className='h-full sm:bg-green-500 absolute'
-                      style={{ width: '10%' }}
+                      className="h-full sm:bg-green-500 absolute"
+                      style={{ width: "10%" }}
                     ></div>
                   </div>
                   <span>10%</span>
@@ -142,45 +144,45 @@ function OrdersTableItems({ data, ordersList, setList }) {
             ))}
           </tbody>
         ) : (
-          <tbody className='text-sm divide-y divide-slate-200'>
+          <tbody className="text-sm divide-y divide-slate-200">
             {dataFilter?.map((item, index) => (
               <tr key={index}>
-                <td className='px-2 first:pl-5 py-3 whitespace-nowrap md:w-1/6'>
-                  <p className='text-textTableItem font-medium capitalize'>
+                <td className="px-2 first:pl-5 py-3 whitespace-nowrap md:w-1/6">
+                  <p className="text-textTableItem font-medium capitalize">
                     {item?.item}
                   </p>
-                  <span className='text-primary'>{item?.category}</span>
+                  <span className="text-primary">{item?.category}</span>
                 </td>
-                <td className='py-3 whitespace-nowrap'>
-                  <p className='text-center'>{item?.org}</p>
+                <td className="py-3 whitespace-nowrap">
+                  <p className="text-center">{item?.org}</p>
                 </td>
-                <td className='py-3 whitespace-nowrap md:w-1/6'>
-                  <p className='font-medium capitalize text-center'>
+                <td className="py-3 whitespace-nowrap md:w-1/6">
+                  <p className="font-medium capitalize text-center">
                     {formatNumber(item?.minimum_quantity)}
                   </p>
                 </td>
-                <td className='py-3 whitespace-nowrap md:w-1/6'>
-                  <p className='font-medium text-center'>
+                <td className="py-3 whitespace-nowrap md:w-1/6">
+                  <p className="font-medium text-center">
                     {formatNumber(item?.maximum_quantity)}
                   </p>
                 </td>
-                <td className='py-3 whitespace-nowrap md:w-1/6'>
-                  <p className='text-center'>
+                <td className="py-3 whitespace-nowrap md:w-1/6">
+                  <p className="text-center">
                     {formatNumber(item?.on_hand_quantity)}
                   </p>
                 </td>
-                <td className='py-3 whitespace-nowrap'>
-                  <p className='text-center'>
+                <td className="py-3 whitespace-nowrap">
+                  <p className="text-center">
                     {formatNumber(item?.reorder_quantity)}
                   </p>
                 </td>
 
-                <td className='py-3 whitespace-nowrap px-2'>
-                  <div className='mt-2 h-4 relative rounded-full overflow-hidden border border-slate-300'>
-                    <div className='w-full h-full bg-gray-200 absolute '></div>
+                <td className="py-3 whitespace-nowrap px-2">
+                  <div className="mt-2 h-4 relative rounded-full overflow-hidden border border-slate-300">
+                    <div className="w-full h-full bg-gray-200 absolute "></div>
                     <div
-                      className='h-full sm:bg-green-500 absolute'
-                      style={{ width: '10%' }}
+                      className="h-full sm:bg-green-500 absolute"
+                      style={{ width: "10%" }}
                     ></div>
                   </div>
                   <span>10%</span>
--- src/partials/planning/plannings/PlanningsTable.jsx
+++ src/partials/planning/plannings/PlanningsTable.jsx
@@ -1,11 +1,10 @@
-import PlanningsTableItems from './PlanningsTableItems';
-import { useDispatch, useSelector } from 'react-redux';
-import { orderAsc, orderDesc } from '../../capabilities/orderFunc';
-import { useEffect, useState } from 'react';
-import icons from '../../../images/icon/icons';
-import DropdownFilter from '../../../components/DropdownFilter';
+import PlanningsTableItems from "./PlanningsTableItems";
+import { useDispatch, useSelector } from "react-redux";
+import { orderAsc, orderDesc } from "../../capabilities/orderFunc";
+import { useEffect, useState } from "react";
+import icons from "../../../images/icon/icons";
+import DropdownFilter from "../../../components/DropdownFilter";
 import {
-  getListHistory,
   revertSearch,
   selectHistorySearch,
   selectListHistory,
@@ -13,25 +12,14 @@ import {
   setSearch,
   selectPlanningList,
   getPlanningList,
-} from '../../../store/slice/planningSlice';
-import Loading from '../../../pages/component/Loading';
-import { selectGroup } from '../../../store/slice/capabilitiesSlice';
+} from "../../../store/slice/planningSlice";
+import Loading from "../../../pages/component/Loading";
+import { selectGroup } from "../../../store/slice/capabilitiesSlice";
 
 function PlanningsTable({ setGroupOptionsPanel }) {
-  const [orderItem, setOrderItem] = useState({ state: false, asc: false });
-  const [orderUser, setOrderUser] = useState({ state: false, asc: false });
-  const [orderQuantity, setOrderQuantity] = useState({
-    state: false,
-    asc: false,
-  });
   const [startSearch, setStartSearch] = useState(false);
-  const [loadData, setLoadData] = useState(true);
-
   const dispatch = useDispatch();
-  const listHistory = useSelector(selectListHistory);
-  const load = useSelector(selectLoadHistory);
   const searchItems = useSelector(selectHistorySearch);
-  const groups = useSelector(selectGroup);
   const planningsList = useSelector(selectPlanningList);
 
   const [planningListOrder, setPlanningListOrder] = useState(
@@ -46,16 +34,105 @@ function PlanningsTable({ setGroupOptionsPanel }) {
     setPlanningListOrder(planningsList);
   }, [planningsList]);
 
-  useEffect(() => {
-    setLoadData(load);
-  }, [load]);
+  const handleSearch = (e) => {
+    if (e.target.value.length > 0) {
+      setStartSearch(true);
+      let result = planningListOrder.filter((element) => {
+        if (
+          element.order_item
+            //.toString()
+            .toLowerCase()
+            .includes(e.target.value.toLowerCase())
+        ) {
+          return element;
+        }
+      });
+      dispatch(setSearch(result));
+    } else {
+      dispatch(revertSearch());
+      setStartSearch(false);
+    }
+  };
+
+  return (
+    <>
+      <section className="mb-5 flex justify-between">
+        <div className="mb-4 sm:mb-0">
+          <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">
+            Planeaciones
+          </h1>
+        </div>
+        <div className="flex space-x-3 z-50">
+          <DropdownFilter />
+
+          <input
+            className="form-input w-72"
+            placeholder="Buscar por Item..."
+            type="search"
+            onChange={handleSearch}
+          />
+          <button
+            type="button"
+            className=" font-medium text-sm bg-white text-primary w-54 space-x-2 border border-primary rounded px-2 flex justify-center items-center"
+          >
+            <img className="w-5" src={icons.graphGantt} alt="Gantt" />
+            <span>Vista de Gantt</span>
+          </button>
+          <button
+            onClick={() => setGroupOptionsPanel(true)}
+            type="button"
+            className="btn bg-primary text-white w-54 space-x-2"
+          >
+            <svg
+              className="w-4 h-4 fill-current opacity-50 shrink-0"
+              viewBox="0 0 16 16"
+            >
+              <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+            </svg>
+            <span>Crear planeación </span>
+          </button>
+        </div>
+      </section>
+      <section className="overflow-x-auto rounded-xl border border-slate-300 h-[550px]">
+        {planningListOrder.length > 0 ? (
+          !startSearch ? (
+            <Table
+              array={planningListOrder}
+              setPlanningListOrder={setPlanningListOrder}
+            />
+          ) : startSearch && searchItems.length > 0 ? (
+            <Table
+              array={searchItems}
+              setPlanningListOrder={setPlanningListOrder}
+            />
+          ) : (
+            <section className="justify-center items-center flex mt-56">
+              <h2 className="font-semibold text-2xl">Sin datos para mostrar</h2>
+            </section>
+          )
+        ) : (
+          <section className="justify-center items-center flex mt-56">
+            <div className="loader"></div>
+            <span className="ml-3 text-primary font-semibold">Cargando</span>
+          </section>
+        )}
+      </section>
+    </>
+  );
+}
+
+const Table = ({ array, setPlanningListOrder }) => {
+  const planningsList = useSelector(selectPlanningList);
+
+  const [orderItem, setOrderItem] = useState({ state: false, asc: false });
+  const [orderUser, setOrderUser] = useState({ state: false, asc: false });
 
   useEffect(() => {
     if (orderItem.state) {
       if (!orderItem.asc) {
-        orderAsc(planningsList, setPlanningListOrder, 'order_item');
+        orderAsc(planningsList, setPlanningListOrder, "order_item");
       } else {
-        orderDesc(planningsList, setPlanningListOrder, 'order_item');
+        orderDesc(planningsList, setPlanningListOrder, "order_item");
       }
     }
   }, [orderItem]);
@@ -63,173 +140,82 @@ function PlanningsTable({ setGroupOptionsPanel }) {
   useEffect(() => {
     if (orderUser.state) {
       if (!orderUser.asc) {
-        orderAsc(planningsList, setPlanningListOrder, 'user');
+        orderAsc(planningsList, setPlanningListOrder, "user");
       } else {
-        orderDesc(planningsList, setPlanningListOrder, 'user');
+        orderDesc(planningsList, setPlanningListOrder, "user");
       }
     }
   }, [orderUser]);
 
-  // useEffect(() => {
-  //   if (orderQuantity.state) {
-  //     if (!orderQuantity.asc) {
-  //       orderAsc(planningsList, setPlanningListOrder, 'order_quantity');
-  //     } else {
-  //       orderDesc(planningsList, setPlanningListOrder, 'order_quantity');
-  //     }
-  //   }
-  // }, [orderQuantity]);
-
-  console.log(planningListOrder);
-  // const handleSearch = (e) => {
-  //   if (e.target.value.length > 0) {
-  //     setStartSearch(true);
-  //     let result = planningListOrder.filter((element) => {
-  //       if (
-  //         element.planning_id
-  //           .toString()
-  //           .toLowerCase()
-  //           .includes(e.target.value.toLowerCase())
-  //       ) {
-  //         return element;
-  //       }
-  //     });
-  //     dispatch(setSearch(result));
-  //   } else {
-  //     dispatch(revertSearch());
-  //     setStartSearch(false);
-  //   }
-  // };
-
   return (
-    <>
-      <section className='mb-5 flex justify-between'>
-        <div className='mb-4 sm:mb-0'>
-          <h1 className='text-2xl md:text-3xl text-slate-800 font-bold'>
-            Planeaciones
-          </h1>
-        </div>
-        <div className='flex space-x-3'>
-          <DropdownFilter />
-          <input
-            className='form-input w-72'
-            placeholder='Buscar por ID...'
-            type='search'
-            // onChange={handleSearch}
-          />
-          <button
-            type='button'
-            className=' font-medium text-sm bg-white text-primary w-54 space-x-2 border border-primary rounded px-2 flex justify-center items-center'
+    <table className="table-auto w-full ">
+      <thead className="text-xs text-textTableHeader font-semibold border-b border-slate-200 bg-slate-50 sticky top-0 z-40">
+        <tr>
+          <th
+            className="px-2 first:pl-5 cursor-pointer"
+            onClick={() => {
+              setOrderItem({ state: true, asc: !orderItem.asc });
+            }}
           >
-            <img className='w-5' src={icons.graphGantt} alt='Gantt' />
-            <span>Vista de Gantt</span>
-          </button>
-          <button
-            onClick={() => setGroupOptionsPanel(true)}
-            type='button'
-            className='btn bg-primary text-white w-54 space-x-2'
-          >
-            <svg
-              className='w-4 h-4 fill-current opacity-50 shrink-0'
-              viewBox='0 0 16 16'
-            >
-              <path d='M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z' />
-            </svg>
-            <span>Crear planeación </span>
-          </button>
-        </div>
-      </section>
-      <section className='overflow-x-auto rounded-xl border border-slate-300 h-[550px]'>
-        <table className='table-auto w-full '>
-          <thead className='text-xs text-textTableHeader font-semibold border-b border-slate-200 bg-slate-50'>
-            <tr>
-              <th
-                className='px-2 first:pl-5 cursor-pointer'
-                onClick={() => {
-                  setOrderItem({ state: true, asc: !orderItem.asc });
-                }}
-              >
-                <div className='flex items-center space-x-2'>
-                  <div className='font-semibold text-left'>Order Item</div>
-                  <img
-                    src={orderItem.asc ? icons.doubleDown : icons.doubleUp}
-                    alt='Flecha abajo'
-                    className='w-5'
-                  />
-                </div>
-              </th>
-              <th
-                className='px-5 first:pl-5 last:pr-5 py-3 whitespace-nowrap cursor-pointer'
-                onClick={() => {
-                  setOrderUser({
-                    state: true,
-                    asc: !orderUser.asc,
-                  });
-                }}
-              >
-                <div className='flex items-center space-x-2'>
-                  <div className='font-semibold'>User</div>
-                  <img
-                    src={orderUser.asc ? icons.doubleDown : icons.doubleUp}
-                    alt='Flecha abajo'
-                    className='w-5'
-                  />
-                </div>
-              </th>
-              <th
-                className='px-4 first:pl-5 last:pr-5 py-3 whitespace-nowrap'
-                // onClick={() => {
-                //   setOrderQuantity({
-                //     state: true,
-                //     asc: !orderQuantity.asc,
-                //   });
-                // }}
-              >
-                <div className='flex items-center justify-center space-x-2'>
-                  <div className='font-semibold'>Order Quantity</div>
-                  {/* <img
-                    src={orderQuantity.asc ? icons.doubleDown : icons.doubleUp}
-                    alt='Flecha abajo'
-                    className='w-5'
-                  /> */}
-                </div>
-              </th>
-              <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
-                <p className='font-semibold text-center'>
-                  Start Production Date
-                </p>
-              </th>
-              <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap'>
-                <p className='font-semibold text-center'>End Production Date</p>
-              </th>
-              <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap'></th>
-
-              {/* <th className='px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap '>
-              <p className='font-semibold text-center'></p>
-            </th> */}
-            </tr>
-          </thead>
-
-          {planningListOrder.map((data) => {
-            return (
-              <PlanningsTableItems
-                key={data.order_planning_id}
-                id={data.order_planning_id}
-                order_item={data.order_item}
-                user={data.user}
-                order_quantity={data.order_quantity}
-                start_production_date={data.start_production_date}
-                end_production_date={data.end_production_date}
-                dem_children={data.dem_childrens}
-                setOrderItem={setOrderItem}
-                orderItem={orderItem}
+            <div className="flex items-center space-x-2">
+              <div className="font-semibold text-left">Order Item</div>
+              <img
+                src={orderItem.asc ? icons.doubleDown : icons.doubleUp}
+                alt="Flecha abajo"
+                className="w-5"
               />
-            );
-          })}
-        </table>
-      </section>
-    </>
+            </div>
+          </th>
+          <th
+            className="px-5 first:pl-5 last:pr-5 py-3 whitespace-nowrap cursor-pointer"
+            onClick={() => {
+              setOrderUser({
+                state: true,
+                asc: !orderUser.asc,
+              });
+            }}
+          >
+            <div className="flex items-center space-x-2">
+              <div className="font-semibold">User</div>
+              <img
+                src={orderUser.asc ? icons.doubleDown : icons.doubleUp}
+                alt="Flecha abajo"
+                className="w-5"
+              />
+            </div>
+          </th>
+          <th className="px-4 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+            <div className="flex items-center justify-center space-x-2">
+              <div className="font-semibold">Order Quantity</div>
+            </div>
+          </th>
+          <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+            <p className="font-semibold text-center">Start Production Date</p>
+          </th>
+          <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+            <p className="font-semibold text-center">End Production Date</p>
+          </th>
+          <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap"></th>
+        </tr>
+      </thead>
+      {array.map((data) => {
+        return (
+          <PlanningsTableItems
+            key={data.order_planning_id}
+            id={data.order_planning_id}
+            order_item={data.order_item}
+            user={data.user}
+            order_quantity={data.order_quantity}
+            start_production_date={data.start_production_date}
+            end_production_date={data.end_production_date}
+            dem_children={data.dem_childrens}
+            setOrderItem={setOrderItem}
+            orderItem={orderItem}
+          />
+        );
+      })}
+    </table>
   );
-}
+};
 
 export default PlanningsTable;
