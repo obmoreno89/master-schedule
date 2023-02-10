e49f6e77082f1efecab3415599172a156cd21c5e
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2023-02-10 14:49:18 -0600

    testing roles

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -378,105 +378,104 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                 }}
               </SidebarLinkGroup>
 
-              {onlyAdministrator === '1' && (
-                <SidebarLinkGroup
-                  activecondition={pathname.includes('management')}
-                >
-                  {(handleClick, open) => {
-                    return (
-                      <React.Fragment>
-                        <Link
-                          to='#0'
-                          className={`block text-textSidebar hover:text-primary truncate transition duration-150 ${
-                            pathname.includes('management') && ' !text-primary'
-                          }`}
-                          onClick={(e) => {
-                            e.preventDefault();
-                            sidebarExpanded
-                              ? handleClick()
-                              : setSidebarExpanded(true);
-                          }}
-                        >
-                          <div className='flex items-center justify-between'>
-                            <figure className='flex items-center'>
-                              <svg
-                                className='shrink-0 h-6 w-6'
-                                viewBox='0 0 24 24'
-                                fill='none'
-                              >
-                                <path
-                                  className={`stroke-current text-textSidebar ${
-                                    pathname.includes('management') &&
-                                    '!text-primary'
-                                  }`}
-                                  d='M17 21V19C17 17.9391 16.5786 16.9217 15.8284 16.1716C15.0783 15.4214 14.0609 15 13 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21'
-                                  strokeWidth='2'
-                                />
-                                <path
-                                  className={`stroke-current text-textSidebar ${
-                                    pathname.includes('management') &&
-                                    '!text-primary'
-                                  }`}
-                                  d='M9 11C11.2091 11 13 9.20914 13 7C13 4.79086 11.2091 3 9 3C6.79086 3 5 4.79086 5 7C5 9.20914 6.79086 11 9 11Z'
-                                  strokeWidth='2'
-                                />
-                                <path
-                                  className={`stroke-current text-textSidebar ${
-                                    pathname.includes('management') &&
-                                    '!text-primary'
-                                  }`}
-                                  d='M23 21V19C22.9993 18.1137 22.7044 17.2528 22.1614 16.5523C21.6184 15.8519 20.8581 15.3516 20 15.13'
-                                  strokeWidth='2'
-                                />
-                                <path
-                                  className={`stroke-current text-textSidebar ${
-                                    pathname.includes('management') &&
-                                    '!text-primary'
-                                  }`}
-                                  d='M16 3.13C16.8604 3.35031 17.623 3.85071 18.1676 4.55232C18.7122 5.25392 19.0078 6.11683 19.0078 7.005C19.0078 7.89318 18.7122 8.75608 18.1676 9.45769C17.623 10.1593 16.8604 10.6597 16 10.88'
-                                  strokeWidth='2'
-                                />
-                              </svg>
-                              <span className='text-sm font-semibold ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200'>
-                                Administración
-                              </span>
-                            </figure>
-                            {/* Icon */}
-                            <div className='flex shrink-0 ml-2'>
-                              <svg
-                                className={`w-3 h-3 shrink-0 ml-1 fill-current text-sideBar ${
-                                  open && 'transform rotate-180'
+              <SidebarLinkGroup
+                activecondition={pathname.includes('management')}
+              >
+                {(handleClick, open) => {
+                  return (
+                    <React.Fragment>
+                      <Link
+                        to='#0'
+                        className={`block text-textSidebar hover:text-primary truncate transition duration-150 ${
+                          pathname.includes('management') && ' !text-primary'
+                        }`}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          sidebarExpanded
+                            ? handleClick()
+                            : setSidebarExpanded(true);
+                        }}
+                      >
+                        <div className='flex items-center justify-between'>
+                          <figure className='flex items-center'>
+                            <svg
+                              className='shrink-0 h-6 w-6'
+                              viewBox='0 0 24 24'
+                              fill='none'
+                            >
+                              <path
+                                className={`stroke-current text-textSidebar ${
+                                  pathname.includes('management') &&
+                                  '!text-primary'
                                 }`}
-                                viewBox='0 0 12 12'
-                              >
-                                <path d='M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z' />
-                              </svg>
-                            </div>
+                                d='M17 21V19C17 17.9391 16.5786 16.9217 15.8284 16.1716C15.0783 15.4214 14.0609 15 13 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21'
+                                strokeWidth='2'
+                              />
+                              <path
+                                className={`stroke-current text-textSidebar ${
+                                  pathname.includes('management') &&
+                                  '!text-primary'
+                                }`}
+                                d='M9 11C11.2091 11 13 9.20914 13 7C13 4.79086 11.2091 3 9 3C6.79086 3 5 4.79086 5 7C5 9.20914 6.79086 11 9 11Z'
+                                strokeWidth='2'
+                              />
+                              <path
+                                className={`stroke-current text-textSidebar ${
+                                  pathname.includes('management') &&
+                                  '!text-primary'
+                                }`}
+                                d='M23 21V19C22.9993 18.1137 22.7044 17.2528 22.1614 16.5523C21.6184 15.8519 20.8581 15.3516 20 15.13'
+                                strokeWidth='2'
+                              />
+                              <path
+                                className={`stroke-current text-textSidebar ${
+                                  pathname.includes('management') &&
+                                  '!text-primary'
+                                }`}
+                                d='M16 3.13C16.8604 3.35031 17.623 3.85071 18.1676 4.55232C18.7122 5.25392 19.0078 6.11683 19.0078 7.005C19.0078 7.89318 18.7122 8.75608 18.1676 9.45769C17.623 10.1593 16.8604 10.6597 16 10.88'
+                                strokeWidth='2'
+                              />
+                            </svg>
+                            <span className='text-sm font-semibold ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200'>
+                              Administración
+                            </span>
+                          </figure>
+                          {/* Icon */}
+                          <div className='flex shrink-0 ml-2'>
+                            <svg
+                              className={`w-3 h-3 shrink-0 ml-1 fill-current text-sideBar ${
+                                open && 'transform rotate-180'
+                              }`}
+                              viewBox='0 0 12 12'
+                            >
+                              <path d='M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z' />
+                            </svg>
                           </div>
-                        </Link>
-                        <div className='lg:hidden lg:sidebar-expanded:block 2xl:block'>
-                          <ul className={`pl-9 mt-1 ${!open && 'hidden'}`}>
-                            <li className='mb-1 last:mb-0'>
-                              <NavLink
-                                end
-                                to='/mp-pro/management/user/'
-                                className={({ isActive }) =>
-                                  'block text-textSidebar hover:text-primary transition duration-150 truncate ' +
-                                  (isActive ? '!text-primary' : '')
-                                }
-                              >
-                                <span className='text-sm  lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200 '>
-                                  Usuarios
-                                </span>
-                              </NavLink>
-                            </li>
-                          </ul>
                         </div>
-                      </React.Fragment>
-                    );
-                  }}
-                </SidebarLinkGroup>
-              )}
+                      </Link>
+                      <div className='lg:hidden lg:sidebar-expanded:block 2xl:block'>
+                        <ul className={`pl-9 mt-1 ${!open && 'hidden'}`}>
+                          <li className='mb-1 last:mb-0'>
+                            <NavLink
+                              end
+                              to='/mp-pro/management/user/'
+                              className={({ isActive }) =>
+                                'block text-textSidebar hover:text-primary transition duration-150 truncate ' +
+                                (isActive ? '!text-primary' : '')
+                              }
+                            >
+                              <span className='text-sm  lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200 '>
+                                Usuarios
+                              </span>
+                            </NavLink>
+                          </li>
+                        </ul>
+                      </div>
+                    </React.Fragment>
+                  );
+                }}
+              </SidebarLinkGroup>
+
               {/* Finance */}
               <SidebarLinkGroup
                 activecondition={
